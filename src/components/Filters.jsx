b3e78866cff49b28f1d69d7d78612d04e10df18d
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-19 16:09:47 +0200

    añadido filtros

---
+++ src/components/Filters.jsx
@@ -0,0 +1,23 @@
+import "./Filters.css";
+
+function Filters() {
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor="price">Price</label>
+        <input type="range" id="price" min="0" max="1000" />
+      </div>
+
+      <div>
+        <label htmlFor="category">Category</label>
+        <select id="category">
+          <option value='all'>All</option>
+          <option value='laptops'>Laptops</option>
+          <option value='smartphones'>Smartphones</option>
+        </select>
+      </div>
+    </section>
+  );
+}
+
+export default Filters;
