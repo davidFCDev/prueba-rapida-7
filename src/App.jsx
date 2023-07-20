0e993014781bad39aa1cd411e6764f3e131608bb
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-20 17:41:45 +0200

    aplicamos context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Products } from "./components/Products";
 import { products as initialProducts } from "./mocks/products.json";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { FiltersContext } from "./context/filters";
 
 function useFilters() {
-  const [filters, setFilters] = useState({
-    category: "all",
-    minPrice: 0,
-  });
+  const filters = useContext(FiltersContext)
+
+  const setFilters = () => {}
 
   const filterProducts = (products) => {
     return products.filter((product) => {
